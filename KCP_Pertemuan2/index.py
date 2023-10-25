48816e742844f30c1ee2cef3aa09d4b4954c52e5
unknown <asmatbaidawi07@gmail.com>
2023-10-25 18:46:06 +0700

    update pratikum

---
+++ KCP_Pertemuan2/index.py
@@ -0,0 +1,29 @@
+#list
+#Kumpulan element yang bisa berubah didefinesikan dengan tanda kurung siki[].
+my_list = [1, 2, 3, 4, 5]
+fruist = ['apple', 'banana', 'cherry']
+mixed_data = [1, 'hello', 3.14, True]
+
+print(my_list)
+print(fruist)
+print(mixed_data)
+
+#Tuple
+#Tuple sama dengan List tetapi tidak bisa diubah. yang didefinesikan dengan tanda kurung bisa()
+my_tuple = (1, 2, 3, 4, 5)
+coordinates = (3.14, 2,71)
+print(my_tuple)
+print(coordinates)
+
+#Set
+#Set kumpulan element unik dan tidak berurutan didefinisikan kurung kurawal atau fungsi set()
+my_set = {1, 2, 3, 4, 5}
+unique_chars = set('hello')
+print(my_set)
+print(unique_chars)
+
+#Dictionary
+#kumpulan eleman yang tidak berurutan didefinisikan menggunakan
+#kurung kurawal {} atau fungsi set().o')
+my_set = {1, 2, 3, 4, 5}
+unique_chars = set('hello')
